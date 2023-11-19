eb91127d77d5c00c950d2b68ecf920be368616c5
Fardeen <fardeenmohd1251@gmail.com>
2023-11-19 13:03:02 +0530

    edited context

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { FilterContext } from './FilterContext.jsx'
-import { Context } from './context.jsx'
+import { Context } from '../src/context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
